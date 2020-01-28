3f5b745cc7c6b4529849e848c24e4de6f4e03f85
Avani Gokhale <36026630+avani112@users.noreply.github.com>
2020-01-28 14:57:43 +0530

    Create simple_queries.sql

---
+++ simple_queries.sql
@@ -0,0 +1,6 @@
+insert into location values(122 ,'New York');
+insert into location values(123 ,'Dallas');
+insert into location values(124 ,'Chicago');
+insert into location values(167 ,'Boston');
+insert into department values(10,'Accounting',122);
+
